de119cb5f7a5fac5802538cf507193eb954d6cd6
Rafael  Carrasco <rafael@Rafaels-MacBook-Pro.local>
2018-03-10 08:38:26 -0600

    Moving Files to Problems Folder

--- P003.py
+++
@@ -1,9 +0,0 @@
-from used_functions import *
-
-"""
-The prime factors of 13195 are 5, 7, 13 and 29.
-
-What is the largest prime factor of the number 600851475143 ?
-"""
-
-print(prime_factor_list(600851475143)[-1])
